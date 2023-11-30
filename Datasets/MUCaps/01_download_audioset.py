79b892224d11e5f094bedce19ec1b8277f77b191
Atin Sakkeer Hussain <atinsaki@gmail.com>
2023-11-30 16:46:29 +0800

    Add MUCaps

---
+++ Datasets/MUCaps/01_download_audioset.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import json
+from tqdm.auto import tqdm
+from yt_dlp import YoutubeDL
+import multiprocessing as mp
+import datetime as dt
+import os
+
+def _download_audio(x):
+    (
+        ytid,
+        out_dir,
+    ) = x
+    if os.path.exists(f"{out_dir}/{ytid}.mp3"):
+        return
+    ydl_opts = {
+        "outtmpl": f"{out_dir}/{ytid}.%(ext)s",
+        "format": "bestaudio[ext=webm]/bestaudio/best",
+        "external_downloader": "ffmpeg",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+            }
+        ],
+        "quiet": True,
+        "no-mtime": True,
+    }
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([f"https://www.youtube.com/watch?v={ytid}"])
+    except KeyboardInterrupt:
+        raise
+    except Exception:
+        pass
+
+
+def download_ps(ytid, save_path, num_processes=None, desc=None):
+    with mp.Pool(processes=num_processes) as pool, tqdm(total=len(ytid), desc=desc) as pbar:
+        for _ in tqdm(
+                pool.imap(
+                    _download_audio,
+                    zip(ytid, [save_path] * len(ytid)),
+                ),
+                total=len(ytid),
+        ):
+            pbar.update()
+
+
+if __name__ == "__main__":
+    ontologies = json.load(open('ontology.json', 'r'))
+
+    ont_data = {}
+
+    for ont in ontologies:
+        ont_data[ont["id"]] = ont["name"].lower()
+
+    df = pd.read_csv("audioset.csv")
+
+    count = 0
+    ids = []
+    for i, row in tqdm(df.iterrows(), total=len(df)):
+        valid = False
+        for idx in row["positive_labels"].split(","):
+            if "musical instrument" in ont_data[idx]:
+                valid = True
+            if "human" in ont_data[idx] \
+                    or "conversation" in ont_data[idx] \
+                    or "sing" in ont_data[idx] \
+                    or "vocal" in ont_data[idx]:
+                valid = False
+        if valid:
+            count += 1
+            ids.append(i)
+
+    df = df.iloc[ids]
+    print(f"Total Count: {len(df['YTID'].unique())}")
+    download_ps(df["YTID"].unique()[80000:], "./audioset_full")
--- M2UGen/data/dataset.py
+++ M2UGen/data/dataset.py
@@ -3,19 +3,6 @@ from .dec_datasets import *
 from .instruction_datasets import *
 from torch.utils.data import ConcatDataset
 
-PROMPT_DICT = {
-    "prompt_input": (
-        "Below is an instruction that describes a task, paired with an input that provides further context. "
-        "Write a response that appropriately completes the request.\n\n"
-        "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:"
-    ),
-    "prompt_no_input": (
-        "Below is an instruction that describes a task. "
-        "Write a response that appropriately completes the request.\n\n"
-        "### Instruction:\n{instruction}\n\n### Response:"
-    ),
-}
-
 
 class FinetuneDataset(Dataset):
     def __init__(self, max_words=30, tokenizer=None, stage=1):
@@ -23,12 +10,12 @@ class FinetuneDataset(Dataset):
 
         if stage == 1:
             # Encoder Datasets
-            mucaps = MUCapsDataset("./Datasets/MUCaps/MUCapsCaptions.json",
-                                   "./Datasets/MUCaps/audios/", "AudioToText", tokenizer, max_words)
-            coco = COCODataset("./Datasets/COCO/COCOCaptions.json",
-                               "./Datasets/COCO/train2014/", "ImageToText", tokenizer, max_words)
-            videocaps = VideoCapsDataset("./Datasets/MUVideo/MUVideoCaptions.json",
-                                         "./Datasets/MUVideo/audioset_video/", "VideoToText",
+            mucaps = MUCapsDataset("../Datasets/MUCaps/MUCapsCaptions.json",
+                                   "../Datasets/MUCaps/audios/", "AudioToText", tokenizer, max_words)
+            coco = COCODataset("../Datasets/COCO/COCOCaptions.json",
+                               "../Datasets/COCO/train2014/", "ImageToText", tokenizer, max_words)
+            videocaps = VideoCapsDataset("../Datasets/MUVideo/MUVideoCaptions.json",
+                                         "../Datasets/MUVideo/audioset_video/", "VideoToText",
                                          tokenizer, max_words)
             dataset_list.append(mucaps)
             dataset_list.append(coco)
@@ -36,33 +23,33 @@ class FinetuneDataset(Dataset):
 
         if stage == 2:
             # Decoder Dataset
-            mucaps_decoder = MUCapsDecoderDataset("./Datasets/MUCaps/MUCapsCaptions.json",
-                                                  "./Datasets/MUCaps/audios/", "TextToAudio",
+            mucaps_decoder = MUCapsDecoderDataset("../Datasets/MUCaps/MUCapsCaptions.json",
+                                                  "../Datasets/MUCaps/audios/", "TextToAudio",
                                                   tokenizer, max_words)
             dataset_list.append(mucaps_decoder)
 
         if stage == 3:
             # QA Dataset
-            musicqa = MusicQADataset("./Datasets/MusicQA/MusicQA.json",
-                                     "./Datasets/MusicQA/audios", "AudioToText", tokenizer,
+            musicqa = MusicQADataset("../Datasets/MusicQA/MusicQA.json",
+                                     "../Datasets/MusicQA/audios", "AudioToText", tokenizer,
                                      max_words)
 
             # Text Instruction
-            alpaca = AlpacaDataset("./Datasets/Alpaca/alpaca_data.json", "TextToText", tokenizer,
+            alpaca = AlpacaDataset("../Datasets/Alpaca/alpaca_data.json", "TextToText", tokenizer,
                                    max_words)
 
             # Generation Instruction Datasets
-            muimage = AnyToMusicInstructionDataset("./Datasets/MUImage/MUImageInstructions.json",
-                                                   "./Datasets/MUImage/audioset_images",
-                                                   "./Datasets/MUImage/audioset",
+            muimage = AnyToMusicInstructionDataset("../Datasets/MUImage/MUImageInstructions.json",
+                                                   "../Datasets/MUImage/audioset_images",
+                                                   "../Datasets/MUImage/audioset",
                                                    "ImageToAudio", tokenizer, max_words)
-            muvideo = AnyToMusicInstructionDataset("./Datasets/MUVideo/MUVideoInstructions.json",
-                                                   "./Datasets/MUVideo/audioset_video",
-                                                   "./Datasets/MUVideo/audioset",
+            muvideo = AnyToMusicInstructionDataset("../Datasets/MUVideo/MUVideoInstructions.json",
+                                                   "../Datasets/MUVideo/audioset_video",
+                                                   "../Datasets/MUVideo/audioset",
                                                    "VideoToAudio", tokenizer, max_words)
-            muedit = AnyToMusicInstructionDataset("./Datasets/MUEdit/MUEditInstructions.json",
-                                                  "./Datasets/MUEdit/audioset",
-                                                  "./Datasets/MUEdit/audioset",
+            muedit = AnyToMusicInstructionDataset("../Datasets/MUEdit/MUEditInstructions.json",
+                                                  "../Datasets/MUEdit/audioset",
+                                                  "../Datasets/MUEdit/audioset",
                                                   "AudioToAudio", tokenizer, max_words)
             dataset_list.append(musicqa)
             dataset_list.append(alpaca)
