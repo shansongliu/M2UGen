a08e82aeeb276dd1458aa83c9b9b3aa4419fa11a
Atin Sakkeer Hussain <atinsaki@gmail.com>
2023-12-16 20:41:52 +0400

    Add MusicGen and AudioLDM2

---
+++ Evaluation/Text2Music/generate_audioldm2.py
@@ -0,0 +1,32 @@
+import json
+from tqdm import tqdm
+from diffusers import AudioLDM2Pipeline
+import os
+from pydub import AudioSegment
+import scipy
+import torch
+
+data = json.load(open("../../Datasets/MUCaps/MUCapsEvalCaptions.json"))
+
+model = AudioLDM2Pipeline.from_pretrained("cvssp/audioldm2", torch_dtype=torch.float16)
+model = model.to("cuda")
+
+
+def generate(prompt, length_in_sec=10):
+    audio = model(
+        prompt,
+        negative_prompt='Low Quality',
+        num_inference_steps=200,
+        audio_length_in_s=length_in_sec,
+        num_waveforms_per_prompt=3
+    ).audios
+    return audio
+
+
+if not os.path.exists("./results/audioldm2"):
+    os.makedirs("./results/audioldm2")
+
+for music, caption in tqdm(data):
+    audioSegment = AudioSegment.from_wav(os.path.join("../../Datasets/MUCaps/audios_eval", music))
+    audio = generate(caption, length_in_sec=audioSegment.duration_seconds)
+    scipy.io.wavfile.write(f"./results/audioldm2/{music}", rate=16000, data=audio[0])
---
+++ Evaluation/Text2Music/generate_musicgen.py
@@ -0,0 +1,44 @@
+import json
+from tqdm import tqdm
+from transformers import AutoProcessor, MusicgenForConditionalGeneration
+import os
+import numpy as np
+from pydub import AudioSegment
+import scipy
+
+data = json.load(open("../../Datasets/MUCaps/MUCapsEvalCaptions.json"))
+
+processor = AutoProcessor.from_pretrained("facebook/musicgen-medium")
+model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-medium")
+model = model.to("cuda")
+
+
+def generate(prompt, audio=None, length_in_sec=10):
+    sampling_rate = model.config.audio_encoder.sampling_rate
+    token_length = int(256*length_in_sec/5)
+    start = 0
+    audios = []
+    while start < token_length:
+        length = min(256*4, token_length - start)
+        inputs = processor(
+            audio=audio,
+            sampling_rate=sampling_rate,
+            text=[prompt],
+            padding=True,
+            return_tensors="pt",
+        ).to("cuda")
+        audio_values = model.generate(**inputs, guidance_scale=3.5, max_new_tokens=length)
+        audios.append(audio_values[0].cpu().numpy()[0])
+        audio = audios[-1][:10*sampling_rate]
+        start += length
+    audio = np.concatenate(audios, axis=0)
+    return audio
+
+
+if not os.path.exists("./results/musicgen"):
+    os.makedirs("./results/musicgen")
+
+for music, caption in tqdm(data):
+    audioSegment = AudioSegment.from_wav(os.path.join("../../Datasets/MUCaps/audios_eval", music))
+    audio = generate(caption, length_in_sec=audioSegment.duration_seconds)
+    scipy.io.wavfile.write(f"./results/musicgen/{music}", rate=model.config.audio_encoder.sampling_rate, data=audio)
