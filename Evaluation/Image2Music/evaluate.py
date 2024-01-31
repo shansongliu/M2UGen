f2ecaae9b320e9b63c4820127adbef823ac8571a
Atin Sakkeer Hussain <atinsaki@gmail.com>
2024-01-31 20:09:16 +0800

    Edit FAD Metric Calculation

--- Evaluation/Image2Music/evaluate.py
+++ Evaluation/Image2Music/evaluate.py
@@ -1,4 +1,4 @@
-from frechet_audio_distance import FrechetAudioDistance
+import subprocess
 from hear21passt.base import get_basic_model, get_model_passt
 import librosa
 from sklearn.metrics import mutual_info_score
@@ -6,6 +6,7 @@ import torch
 from pathlib import Path
 import json
 import sys
+import re
 
 from tqdm import tqdm
 
@@ -19,11 +20,6 @@ models = ['CoDi', 'M2UGen_v1', 'M2UGen_v2']
 scores = {model: {"FAD": 0, "CLAP": 0, "IM_RANK": 0} for model in models}
 model_files = {"CoDi": "./results/codi", "M2UGen_v1": "./results/m2ugen_v1", "M2UGen_v2": "./results/m2ugen_v2"}
 model_order = {k: v for k, v in enumerate(models)}
-
-frechet = FrechetAudioDistance(model_name="vggish",
-                               use_pca=False,
-                               use_activation=False,
-                               verbose=True)
 
 kl_model = get_basic_model(mode="logits")
 kl_model.eval()
@@ -77,10 +73,20 @@ def kl_divergence(pred_probs: torch.Tensor, target_probs: torch.Tensor, epsilon:
     return kl_div.sum(-1).mean()
 
 
+def fad_score(original, generated):
+    print("Calculating FAD Score...")
+    command = f"fad_embed --verbose vggish {original} {generated}".split(" ")
+    subprocess.run(command)
+    command2 = f"fad_score {original}_emb_vggish {generated}_emb_vggish".split(" ")
+    result2 = subprocess.run(command2, stdout=subprocess.PIPE)
+    match = re.search("FAD score\s=\s+(\d*\.?\d*)", result2.stdout.decode())
+    return float(match.group(1))
+
+
 data = json.load(open("../../Datasets/MUImage/MUImageEvalInstructions.json"))
 
 for model in scores.keys():
-    scores[model]["FAD"] = frechet.score("../../Datasets/MUImage/audios_eval", model_files[model])
+    scores[model]["FAD"] = fad_score("../../Datasets/MUImage/audios_eval", model_files[model])
     generated_files = [str(x).split("/")[-1] for x in Path(model_files[model]).glob("*.mp3")]
     target_prob, pred_prob = [], []
     for music in generated_files:
--- Evaluation/Text2Music/evaluate.py
+++ Evaluation/Text2Music/evaluate.py
@@ -1,4 +1,5 @@
-from frechet_audio_distance import FrechetAudioDistance
+import re
+import subprocess
 from hear21passt.base import get_basic_model, get_model_passt
 import librosa
 from sklearn.metrics import mutual_info_score
@@ -11,11 +12,6 @@ scores = {model: {"FAD": 0, "CLAP": 0, "KL": 0} for model in
           ['MusicGen', 'AudioLDM2', 'CoDi', 'M2UGen_v1', 'M2UGen_v2']}
 model_files = {"MusicGen": "./results/musicgen", "AudioLDM2": "./results/audioldm2", "CoDi": "./results/codi",
                "M2UGen_v1": "./results/m2ugen_v1", "M2UGen_v2": "./results/m2ugen_v2"}
-
-frechet = FrechetAudioDistance(model_name="vggish",
-                               use_pca=False,
-                               use_activation=False,
-                               verbose=True)
 
 kl_model = get_basic_model(mode="logits")
 kl_model.eval()
@@ -61,10 +57,20 @@ def kl_divergence(pred_probs: torch.Tensor, target_probs: torch.Tensor, epsilon:
     return kl_div.sum(-1).mean()
 
 
+def fad_score(original, generated):
+    print("Calculating FAD Score...")
+    command = f"fad_embed --verbose vggish {original} {generated}".split(" ")
+    subprocess.run(command)
+    command2 = f"fad_score {original}_emb_vggish {generated}_emb_vggish".split(" ")
+    result2 = subprocess.run(command2, stdout=subprocess.PIPE)
+    match = re.search("FAD score\s=\s+(\d*\.?\d*)", result2.stdout.decode())
+    return float(match.group(1))
+
+
 data = json.load(open("../../Datasets/MUCaps/MUCapsEvalCaptions.json"))
 
 for model in scores.keys():
-    scores[model]["FAD"] = frechet.score("../../Datasets/MUCaps/audios_eval", model_files[model])
+    scores[model]["FAD"] = fad_score("../../Datasets/MUCaps/audios_eval", model_files[model])
     generated_files = [str(x).split("/")[-1] for x in Path(model_files[model]).glob("*.mp3")]
     target_prob, pred_prob = [], []
     for music in generated_files:
