673565af454f9804889ea21b4e7ebf801a0aeee1
Atin Sakkeer Hussain <atinsaki@gmail.com>
2024-03-03 06:04:20 +0800

    Update 01_download_audioset.py

--- Datasets/MUVideo/01_download_audioset.py
+++ Datasets/MUVideo/01_download_audioset.py
@@ -71,6 +71,6 @@ def download_ps(ytid, starts, ends, save_path, num_processes=None, desc=None):
 if __name__ == "__main__":
     df = pd.read_csv("../common/filtered.csv")
     print(f"Total Count: {len(df['YTID'].unique())}")
-    download_ps(df["YTID"][20000:25000],
+    download_ps(df["YTID"][40000:50000],
                 [x * 1000 for x in df["start_seconds"][40000:50000]],
                 [x * 1000 for x in df["end_seconds"][40000:50000]], "./audioset")
