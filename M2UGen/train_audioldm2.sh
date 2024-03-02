cc9a669e8e7219d0fdf3a9806d2556093b27a837
Atin Sakkeer Hussain <atinsaki@gmail.com>
2024-03-03 06:17:16 +0800

    Update train_audioldm2.sh

--- M2UGen/train_audioldm2.sh
+++ M2UGen/train_audioldm2.sh
@@ -7,7 +7,7 @@ mkdir -p "$OUTPUT_DIR"
 
  python3 -u -m torch.distributed.launch --master_port=1114 --nproc_per_node=1 --use_env \
  main_train.py --batch_size 1 --accum_iter 4 --stage 1 --music_decoder audioldm2 \
- --epochs 2 --split_epoch 1 --warmup_epochs 0 --lr 1e-5 --weight_decay 0.05 \
+ --epochs 5 --split_epoch 1 --warmup_epochs 0 --lr 1e-4 --min_lr 1e-6 --weight_decay 0.05 \
  --mert_path /hpctmp/e0589920/MERT-v1-330M/ --vit_path /hpctmp/e0589920/ViT \
  --vivit_path /hpctmp/e0589920/ViViT --music_decoder_path /hpctmp/e0589920/audioldm2-music \
  --llama_path "$LLAMA_PATH" \
@@ -15,7 +15,7 @@ mkdir -p "$OUTPUT_DIR"
 
  python3 -u -m torch.distributed.launch --master_port=1114 --nproc_per_node=1 --use_env \
   main_train.py --batch_size 1 --accum_iter 4 --stage 2 --music_decoder audioldm2 \
-  --epochs 2 --split_epoch 1 --warmup_epochs 5 --lr 1e-6 --weight_decay 0.05 \
+  --epochs 5 --split_epoch 1 --warmup_epochs 0 --lr 1e-4 --min_lr 1e-6 --weight_decay 0.05 \
   --mert_path /hpctmp/e0589920/MERT-v1-330M/ --vit_path /hpctmp/e0589920/ViT \
   --vivit_path /hpctmp/e0589920/ViViT --music_decoder_path /hpctmp/e0589920/audioldm2-music \
   --llama_path "$LLAMA_PATH" \
@@ -23,7 +23,7 @@ mkdir -p "$OUTPUT_DIR"
 
  python3 -u -m torch.distributed.launch --master_port=1114 --nproc_per_node=1 --use_env \
  main_train.py --batch_size 1 --accum_iter 4 --stage 3 --music_decoder audioldm2 \
- --epochs 5 --split_epoch 1 --warmup_epochs 0 --lr 1e-5 --min_lr 0 --weight_decay 0.05 \
+ --epochs 2 --split_epoch 1 --warmup_epochs 0 --lr 1e-4 --min_lr 1e-6 --weight_decay 0.05 \
  --mert_path /hpctmp/e0589920/MERT-v1-330M/ --vit_path /hpctmp/e0589920/ViT \
  --vivit_path /hpctmp/e0589920/ViViT --music_decoder_path /hpctmp/e0589920/audioldm2-music \
  --llama_path "$LLAMA_PATH" \
